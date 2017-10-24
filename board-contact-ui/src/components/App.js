14dc9be3aef71a21521f8fa1d480357463109d4c
shameelkh <shameel-khan@hotmail.com>
2017-10-24 08:00:40 -0400

    Add basic routing

--- board-contact-ui/src/components/App.js
+++ board-contact-ui/src/components/App.js
@@ -1,20 +1,26 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import BoardInfoPage from '../containers/BoardInfoPage'
 import ContactsPage from '../containers/ContactsPage'
 import Search from '../containers/Search'
 
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-
 const App = () => {
     return (
-        <div>
-            <h1>School Boards Data Center</h1>
-            <Search/>
-            <hr/>
-            <BoardInfoPage/>
-            <hr/>
-            <ContactsPage/>
-        </div>
+        <Router>
+            <div>
+                <h1>School Boards Data Center</h1>
+                <Search/>
+
+                <div>
+                    <Link to={'/basic-info'}>Basic Info</Link> |
+                    <Link to={'contacts'}>Contacts</Link>
+                </div>
+
+                <Route path="/basic-info" component={BoardInfoPage} />
+                <Route path="/contacts" component={ContactsPage} />
+            </div>
+        </Router>
     )
 }
 
--- board-contact-ui/src/components/Root.js
+++ board-contact-ui/src/components/Root.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import App from './App'
+
 
 const Root = ({store}) => {
     return (
         <Provider store={store}>
-            <App/>
+            <Router>
+                <Route path="/" component={App} />
+            </Router>
         </Provider>
     )
 }
