1f33c8e8e61862ed58eb8c07a97375f19d045600
shameelkh <shameel-khan@hotmail.com>
2017-10-29 14:36:45 -0400

    Update EditProfileForm to a class level component

--- board-contact-ui/src/components/EditProfileForm.js
+++ board-contact-ui/src/components/EditProfileForm.js
@@ -1,45 +1,66 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
-const EditProfileForm = ({boardInfo}) => {
+class EditProfileForm extends React.Component {
 
-    let board = boardInfo.board
+    constructor(props) {
+        super(props)
 
-    if (board === null || board === undefined) {
-        return <div>Nothing to Show</div>
+        this.state = {
+            street: '',
+            city: '',
+            postalCode: '',
+            phoneNumber: undefined,
+            website: '',
+            openTime: '',
+            closeTime: ''
+        }
     }
 
-    return (
-        <form>
-            <div className="group-section">
-                <div class="field-section">
-                    <input type="text" value={board.address.street + ', ' + board.address.city + ', ' + board.address.postalCode} />
-                    <div><span class="field-name">Address</span></div>
-                </div>
-            </div>
+    render() {
+        let board = this.props.boardInfo.board
 
-            <div className="group-section">
-                <div class="field-section">
-                    <input type="text" class="field-content" value={board.phoneNumber} />
-                    <div><span class="field-name">Phone</span></div>
-                </div>
-                <div class="field-section">
-                    <input type="text" class="field-content" value={board.website} />
-                    <div><span class="field-name">Website</span></div>
-                </div>
-            </div>
+        if (board === null || board === undefined) {
+            return <div>Nothing to Show</div>
+        }
 
-            <div className="group-section">
-                 <div class="field-section">
-                    <input type="text" class="field-content" value={board.openTime} />
-                    <div><span class="field-name">Open Time</span></div>
-                 </div>
-                 <div class="field-section">
-                    <input type="text" class="field-content" value={board.closeTime} />
-                    <div><span class="field-name">Close Time</span></div>
-                 </div>
-            </div>
-        </form>
-    )
+        return (
+            <form>
+                <div className="group-section">
+                    <div class="field-section">
+                        <input type="text" value={board.address.street + ', ' + board.address.city + ', ' + board.address.postalCode} />
+                        <div><span class="field-name">Address</span></div>
+                    </div>
+                </div>
+
+                <div className="group-section">
+                    <div class="field-section">
+                        <input type="text" class="field-content" value={board.phoneNumber} />
+                        <div><span class="field-name">Phone</span></div>
+                    </div>
+                    <div class="field-section">
+                        <input type="text" class="field-content" value={board.website} />
+                        <div><span class="field-name">Website</span></div>
+                    </div>
+                </div>
+
+                <div className="group-section">
+                     <div class="field-section">
+                        <input type="text" class="field-content" value={board.openTime} />
+                        <div><span class="field-name">Open Time</span></div>
+                     </div>
+                     <div class="field-section">
+                        <input type="text" class="field-content" value={board.closeTime} />
+                        <div><span class="field-name">Close Time</span></div>
+                     </div>
+                </div>
+            </form>
+        )
+    }
+}
+
+EditProfileForm.propTypes = {
+    boardInfo: PropTypes.object.isRequired
 }
 
 export default EditProfileForm
