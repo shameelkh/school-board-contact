8d630350d203d1c8342d578827aca23b8fc2a7ff
shameelkh <shameel-khan@hotmail.com>
2017-10-31 07:21:41 -0400

    update Contact component layout and styles

--- board-contact-ui/src/components/Contact.js
+++ board-contact-ui/src/components/Contact.js
@@ -2,9 +2,33 @@ import React from 'react'
 
 const Contact = ({contact}) => {
     return (
-        <div>
-            <p>Name: {contact.firstName} {contact.lastName} | Email: {contact.email}</p>
-            <p>Is Primary: {contact.isPrimary ? 'Yes' : 'No'}</p>
+        <div className="contact-section">
+            <div className="group-section-contact">
+                <div class="field-section-contact">
+                    <span class="field-content">{contact.salutation + ' ' + contact.firstName + ' ' + contact.lastName}</span>
+                    <div><span class="field-name">Name</span></div>
+                </div>
+            </div>
+            <div className="group-section-contact">
+                <div class="field-section-contact">
+                    <span class="field-content">{contact.title}</span>
+                    <div><span class="field-name">Title</span></div>
+                </div>
+                 <div class="field-section-contact">
+                    <span class="field-content">{contact.email}</span>
+                    <div><span class="field-name">Email</span></div>
+                </div>
+            </div>
+            <div className="group-section-contact">
+                <div class="field-section-contact">
+                    <span class="field-content">{contact.phoneNumber}</span>
+                    <div><span class="field-name">Phone</span></div>
+                </div>
+                <div class="field-section-contact">
+                    <span class="field-content">{contact.isPrimary ? 'Yes' : 'No'}</span>
+                    <div><span class="field-name">Primary</span></div>
+                </div>
+            </div>
         </div>
     )
 }
