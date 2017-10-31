ab8d74ebbb28e883562dfc9a487afa090ff65e89
shameelkh <shameel-khan@hotmail.com>
2017-10-31 07:30:54 -0400

    Add expand and collapse feature

--- board-contact-ui/src/components/Contact.js
+++ board-contact-ui/src/components/Contact.js
@@ -1,36 +1,69 @@
 import React from 'react'
 
-const Contact = ({contact}) => {
-    return (
-        <div className="contact-section">
-            <div className="group-section-contact">
-                <div class="field-section-contact">
-                    <span class="field-content">{contact.salutation + ' ' + contact.firstName + ' ' + contact.lastName}</span>
-                    <div><span class="field-name">Name</span></div>
-                </div>
+class Contact extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            isExpanded: false
+        }
+    }
+
+    handleClick = () => {
+        this.setState({
+            isExpanded: !this.state.isExpanded
+        })
+    }
+
+    render() {
+        let contact = this.props.contact
+        return (
+            <div>
+                {this.state.isExpanded &&
+                    <div className="contact-section">
+                        <div className="group-section-contact" onClick={this.handleClick}>
+                            <div class="field-section-contact">
+                                <span class="field-content">{contact.salutation + ' ' + contact.firstName + ' ' + contact.lastName}</span>
+                                <div><span class="field-name">Name</span></div>
+                            </div>
+                        </div>
+                        <div className="group-section-contact">
+                            <div class="field-section-contact">
+                                <span class="field-content">{contact.title}</span>
+                                <div><span class="field-name">Title</span></div>
+                            </div>
+                             <div class="field-section-contact">
+                                <span class="field-content">{contact.email}</span>
+                                <div><span class="field-name">Email</span></div>
+                            </div>
+                        </div>
+                        <div className="group-section-contact">
+                            <div class="field-section-contact">
+                                <span class="field-content">{contact.phoneNumber}</span>
+                                <div><span class="field-name">Phone</span></div>
+                            </div>
+                            <div class="field-section-contact">
+                                <span class="field-content">{contact.isPrimary ? 'Yes' : 'No'}</span>
+                                <div><span class="field-name">Primary</span></div>
+                            </div>
+                        </div>
+                    </div>
+                }
+
+                {!this.state.isExpanded &&
+                    <div className="contact-section">
+                        <div className="group-section-contact" onClick={this.handleClick}>
+                            <div class="field-section-contact">
+                                <span class="field-content">{contact.salutation + ' ' + contact.firstName + ' ' + contact.lastName}</span>
+                                <div><span class="field-name">Name</span></div>
+                            </div>
+                        </div>
+                    </div>
+                }
             </div>
-            <div className="group-section-contact">
-                <div class="field-section-contact">
-                    <span class="field-content">{contact.title}</span>
-                    <div><span class="field-name">Title</span></div>
-                </div>
-                 <div class="field-section-contact">
-                    <span class="field-content">{contact.email}</span>
-                    <div><span class="field-name">Email</span></div>
-                </div>
-            </div>
-            <div className="group-section-contact">
-                <div class="field-section-contact">
-                    <span class="field-content">{contact.phoneNumber}</span>
-                    <div><span class="field-name">Phone</span></div>
-                </div>
-                <div class="field-section-contact">
-                    <span class="field-content">{contact.isPrimary ? 'Yes' : 'No'}</span>
-                    <div><span class="field-name">Primary</span></div>
-                </div>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Contact
