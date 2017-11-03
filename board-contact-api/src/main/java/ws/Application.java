990cd3872f1216ce9fafdbff89e9da416664a41d
shameelkh <shameel-khan@hotmail.com>
2017-11-03 16:55:06 -0400

    fix Application.java for adding new contacts

--- board-contact-api/src/main/java/ws/Application.java
+++ board-contact-api/src/main/java/ws/Application.java
@@ -14,6 +14,13 @@ public class Application {
     public static Map<Integer, Employer> employersMap = new HashMap<Integer, Employer>();
     public static Map<Integer, List<Contact>> boardToContactsMap = new HashMap<Integer, List<Contact>>();
 
+    private static int id = 0;
+
+    public static int getNewId() {
+        id++;
+        return id;
+    }
+
     public static void startUp() {
         // Addresses
         Address address1 = new Address(1, "1255 Younge St", "Toronto", "LUT0E3");
@@ -32,25 +39,26 @@ public class Application {
 
         // Create contacts for each school board
 
-        Contact contact1ForBoard1 = new Contact(1, 1, "Mr.", "Tom", "Jerry", "Tom@Jerry.com", "Jr Accountant", address1, "5552229999", true);
-        Contact contact2ForBoard1 = new Contact(2, 1, "Mr.", "Sam", "Foo", "SamFoo@hotmail.com", "Teacher", address1, "4441119999", false);
-        Contact contact3ForBoard1 = new Contact(3, 1, "Ms.", "Cat", "Jul", "Cal@Jul.com", "Manager", address1, "6511234444", false);
+        Contact contact1ForBoard1 = new Contact(getNewId(), 1, "Mr.", "Tom", "Jerry", "Tom@Jerry.com", "Jr Accountant", address1, "5552229999", true);
+        Contact contact2ForBoard1 = new Contact(getNewId(), 1, "Mr.", "Sam", "Foo", "SamFoo@hotmail.com", "Teacher", address1, "4441119999", false);
+        Contact contact3ForBoard1 = new Contact(getNewId(), 1, "Ms.", "Cat", "Jul", "Cal@Jul.com", "Manager", address1, "6511234444", false);
 
-        Contact contact1ForBoard2 = new Contact(4, 2, "Mr.", "Ash", "Catchem", "Gotem@all.com", "Pokemon Trainer", address2, "9993214545", true);
-        Contact contact2ForBoard2 = new Contact(5, 2, "Mr.", "Uch", "Ahh", "Uchahh@gmail.com", "Fake Teacher", address2, "7773331111", false);
-        Contact contact3ForBoard2 = new Contact(6, 2, "Dr.", "John", "Son", "John@Son.com", "Doctor", address2, "4561248844", false);
+        Contact contact1ForBoard2 = new Contact(getNewId(), 2, "Mr.", "Ash", "Catchem", "Gotem@all.com", "Pokemon Trainer", address2, "9993214545", true);
+        Contact contact2ForBoard2 = new Contact(getNewId(), 2, "Mr.", "Uch", "Ahh", "Uchahh@gmail.com", "Fake Teacher", address2, "7773331111", false);
+        Contact contact3ForBoard2 = new Contact(getNewId(), 2, "Dr.", "John", "Son", "John@Son.com", "Doctor", address2, "4561248844", false);
 
-        Contact contact1ForBoard3 = new Contact(7, 3, "Mrs.", "Julie", "Yu", "Julie@Yu.com", "Teacher", address3, "5516763321", true);
-        Contact contact2ForBoard3 = new Contact(8, 3, "Mr.", "Goku", "Son", "Goku@hotmail.com", "Gym Teacher", address3, "8185552121", false);
-        Contact contact3ForBoard3 = new Contact(9, 3, "Mr.", "Pico", "Oky", "Pico@Oky.com", "Random Dude", address3, "8914446666", false);
+        Contact contact1ForBoard3 = new Contact(getNewId(), 3, "Mrs.", "Julie", "Yu", "Julie@Yu.com", "Teacher", address3, "5516763321", true);
+        Contact contact2ForBoard3 = new Contact(getNewId(), 3, "Mr.", "Goku", "Son", "Goku@hotmail.com", "Gym Teacher", address3, "8185552121", false);
+        Contact contact3ForBoard3 = new Contact(getNewId(), 3, "Mr.", "Pico", "Oky", "Pico@Oky.com", "Random Dude", address3, "8914446666", false);
 
-        boardToContactsMap.put(1, Arrays.asList(contact1ForBoard1, contact2ForBoard1, contact3ForBoard1));
-        boardToContactsMap.put(2, Arrays.asList(contact1ForBoard2, contact2ForBoard2, contact3ForBoard2));
-        boardToContactsMap.put(3, Arrays.asList(contact1ForBoard3, contact2ForBoard3, contact3ForBoard3));
+        boardToContactsMap.put(1, new ArrayList<Contact>(Arrays.asList(contact1ForBoard1, contact2ForBoard1, contact3ForBoard1)));
+        boardToContactsMap.put(2, new ArrayList<Contact>(Arrays.asList(contact1ForBoard2, contact2ForBoard2, contact3ForBoard2)));
+        boardToContactsMap.put(3, new ArrayList<Contact>(Arrays.asList(contact1ForBoard3, contact2ForBoard3, contact3ForBoard3)));
     }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         startUp();
     }
+
 }
