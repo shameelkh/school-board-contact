d266e3c7a5608cecdd4e3c268cbb9ceba2c24190
shameelkh <shameel-khan@hotmail.com>
2017-11-18 20:12:08 -0500

    Update AccountMgmtPageContainer to follow the container-presentation model

--- board-contact-ui/src/components/AccountMgmtPage.js
+++ board-contact-ui/src/components/AccountMgmtPage.js
@@ -1,76 +1,42 @@
 import React from 'react'
 import PropTypes from 'prop-types';
-import EditAccountMgmtForm from '../components/EditAccountMgmtForm'
 
-class AccountMgmtPage extends React.Component {
+const AccountMgmtPage = ({board, enableEditMode}) => {
 
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            inEditMode: false
-        }
-    }
-
-    enableEditMode = () => {
-        this.setState({ inEditMode: true })
-    }
-
-    disableEditMode = () => {
-        this.setState({ inEditMode: false })
-    }
-
-    render() {
-        let board = this.props.boardInfo.board
-
-        if (board === undefined) {
-            return (<div></div>)
-        }
-
-        return (
-            <div>
-                {!this.state.inEditMode &&
-                    <div>
-                        <div className="group-section">
-                            <div class="field-section">
-                                <span class="field-content">{board.accountOwner}</span>
-                                <div><span class="field-name">Account Owner</span></div>
-                            </div>
-                        </div>
-                        <div className="group-section">
-                            <div class="field-section">
-                                <span class="field-content">{board.openDate}</span>
-                                <div><span class="field-name">Open Date</span></div>
-                            </div>
-                            <div class="field-section">
-                                <span class="field-content">{board.closeDate}</span>
-                                <div><span class="field-name">Close Date</span></div>
-                            </div>
-                        </div>
-                        <div className="group-section">
-                            <div class="field-section">
-                                <span class="field-content">{board.active ? "Yes" : "No"}</span>
-                                <div><span class="field-name">Active</span></div>
-                            </div>
-                        </div>
-
-                        <button className="btn btn-success" onClick={this.enableEditMode}>Edit</button>
-                    </div>
-                }
-
-                {this.state.inEditMode &&
-                    <EditAccountMgmtForm board={this.props.boardInfo.board}
-                                         disableEditMode={this.disableEditMode}
-                                         saveBoard={this.props.handleSaveBoard} />
-                }
+    return (
+        <div>
+            <div className="group-section">
+                <div class="field-section">
+                    <span class="field-content">{board.accountOwner}</span>
+                    <div><span class="field-name">Account Owner</span></div>
+                </div>
             </div>
-        )
-    }
+            <div className="group-section">
+                <div class="field-section">
+                    <span class="field-content">{board.openDate}</span>
+                    <div><span class="field-name">Open Date</span></div>
+                </div>
+                <div class="field-section">
+                    <span class="field-content">{board.closeDate}</span>
+                    <div><span class="field-name">Close Date</span></div>
+                </div>
+            </div>
+            <div className="group-section">
+                <div class="field-section">
+                    <span class="field-content">{board.active ? "Yes" : "No"}</span>
+                    <div><span class="field-name">Active</span></div>
+                </div>
+            </div>
+
+            <button className="btn btn-success" onClick={enableEditMode}>Edit</button>
+        </div>
+
+    )
 }
 
 AccountMgmtPage.propTypes = {
     board: PropTypes.object.isRequired,
-    handleSaveBoard: PropTypes.func.isRequired
+    enableEditMode: PropTypes.func.isRequired
 }
 
 export default AccountMgmtPage
--- board-contact-ui/src/containers/AccountMgmtPageContainer.js
+++ board-contact-ui/src/containers/AccountMgmtPageContainer.js
@@ -2,7 +2,52 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { saveBoard } from '../actions'
 import AccountMgmtPage from '../components/AccountMgmtPage'
+import EditAccountMgmtForm from '../components/EditAccountMgmtForm'
 
+
+class AccountMgmtPageContainer extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            inEditMode: false
+        }
+    }
+
+    enableEditMode = () => {
+        this.setState({ inEditMode: true })
+    }
+
+    disableEditMode = () => {
+        this.setState({ inEditMode: false })
+    }
+
+    render() {
+        let board = this.props.boardInfo.board
+
+        if (board === null || board === undefined) {
+            return <div>No School Board Selected</div>
+        }
+
+        return (
+            <div>
+                {!this.state.inEditMode &&
+                    <AccountMgmtPage
+                        board={board}
+                        enableEditMode={this.enableEditMode} />
+                }
+
+                {this.state.inEditMode &&
+                    <EditAccountMgmtForm
+                        board={board}
+                        disableEditMode={this.disableEditMode}
+                        saveBoard={this.props.handleSaveBoard} />
+                }
+            </div>
+        )
+    }
+}
 
 const mapStateToProps = (state) => {
     return {
@@ -13,9 +58,9 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
     return {
         handleSaveBoard: (updatedBoard) => {
-            dispatch( saveBoard(updatedBoard) )
+            dispatch(saveBoard(updatedBoard))
         }
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(AccountMgmtPage)
+export default connect(mapStateToProps, mapDispatchToProps)(AccountMgmtPageContainer)
